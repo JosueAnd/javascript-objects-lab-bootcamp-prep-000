7271503502368db33d89a6db8ff62bd25f51aceb
Joshua Andujar <andujar0218@gmail.com>
2018-05-18 13:30:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var recipes = {hello:'world'};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},object,{[key]:value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key, value) {
+  var newObject = Object.assign({}, object, {[key]:value});
+  delete newObject.key;
+  return newObject;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
